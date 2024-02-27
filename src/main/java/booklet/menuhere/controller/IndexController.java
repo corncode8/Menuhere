2616f6aac55ef8f2bd6eb69f8acf3d616d2f5d2f
corncode8 <myeongcheol8@naver.com>
2024-02-27 20:23:40 +0900

    CI/CD test

--- src/main/java/booklet/menuhere/controller/IndexController.java
+++ src/main/java/booklet/menuhere/controller/IndexController.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Slf4j
@@ -20,6 +20,7 @@ public class IndexController {
     }
 
     @GetMapping("/check")
+    @ResponseBody
     public String testpage() {
         return "test success";
     }
